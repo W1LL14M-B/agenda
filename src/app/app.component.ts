d043732426661c18e8489d2967beba148988719a
William <concejo@CB-20583917.CONCEJOBTA.LOC>
2024-10-01 17:43:24 -0500

    Creando tareas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Task } from './models/task.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'agendawb';
+  taskForm: FormGroup;
+  tasks: Task[] = [];
+  taskId: number = 0;
+
+  constructor(private fb: FormBuilder) {
+    this.taskForm = this.fb.group({
+      title: ['', Validators.required],
+      description: [''],
+    });
+  }
 }
+
+
+
