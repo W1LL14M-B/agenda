121ac8b1d83fd33c16364dc3feabe45f11dfa50b
william <wibepu12@gmail.com>
2024-12-06 16:48:26 -0500

    arreglando formulario

--- src/app/models/task.model.ts
+++ src/app/models/task.model.ts
@@ -1,10 +1,21 @@
 import { Person } from "./person.model";
 
-
 export interface Task {
     id: number;
     title: string;
     description: string;
     completed: boolean;
     people: Person[];  
-  }
+  } 
+
+  /*   export interface Task {
+      title: string;
+      description: string;
+      deadline: string; // Tipo string para almacenar fechas en formato ISO
+      completed: boolean;
+      people: Array<{
+        fullName: string;
+        age: number;
+        skills: string[];
+      }>;
+    } */
